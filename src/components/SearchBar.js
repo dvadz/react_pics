91cce6d666625003a9d5884b1f62ffb4a0d79ae7
Daniel Vadil <dvadz@yahoo.com>
2022-03-21 01:13:16 -0400

    Created a basic onSubmit handler with a preventDefault and deleted toUpperCase test code in the render

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,16 +3,20 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { searchTerm: "" };
 
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className='ui segment'>
-        <form className='ui  form'>
+        <form className='ui form' onSubmit={this.onFormSubmit}>
           <input
             type='text'
             placeholder='Search images'
             value={this.state.searchTerm}
             onChange={(e) => {
-              this.setState({ searchTerm: e.target.value.toUpperCase() });
+              this.setState({ searchTerm: e.target.value });
             }}
           />
         </form>
