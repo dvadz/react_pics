fa45abe076533fd3b46b59376757bf94e3c75e46
Daniel Vadil <dvadz@yahoo.com>
2022-03-21 01:44:45 -0400

    Turned App into a class

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import "./App.css";
 
-const App = () => {
-  return (
-    <div className='ui container mt10px'>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div className='ui container mt10px'>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 export default App;
