cba99e4a6bf8c4c4bb21e42ac9daa96282d37956
Daniel Vadil <dvadz@yahoo.com>
2022-03-22 20:39:12 -0400

    Used a grid to layout the images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
+import "./ImageList.css";
 import React from "react";
 
 const ImageList = (props) => {
   const imagesURLs = props.images.map((image, index) => {
     return <img key={image.id} src={image.urls.regular} alt={image.description} />;
   });
-  return <div>{imagesURLs}</div>;
+  return <div className='image-list'>{imagesURLs}</div>;
 };
 
 export default ImageList;
