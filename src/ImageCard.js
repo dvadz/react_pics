9c871fc1a31ced9264df200a46bd22cc4a2867a1
Daniel Vadil <dvadz@yahoo.com>
2022-03-23 10:57:43 -0400

    Added an event listener after the ImageCard has loaded and retrieve its clientHeight

--- src/ImageCard.js
+++ src/ImageCard.js
@@ -6,6 +6,14 @@ class ImageCard extends React.Component {
     this.imageRef = React.createRef();
   }
 
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpan);
+  }
+
+  setSpan = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
   render() {
     return (
       <div>
